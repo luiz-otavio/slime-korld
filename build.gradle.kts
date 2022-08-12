08fa56590f3830a6a9da63dac0adfdeed0bf081a
luiz-otavio <luizfarias2006@hotmail.com>
2022-08-11 22:41:23 -0300

    chore: setup initial workspace

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+subprojects {
+    plugins.apply("java")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    // Change charset to UTF-8
+    tasks.withType<JavaCompile>() {
+        options.encoding = "UTF-8"
+    }
+
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "rewrite-slime-korld"
+
